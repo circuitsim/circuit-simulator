9c942cb39f48f1bb8ec1820a739374a3d0fa93d0
ThomWright <github@thomwright.co.uk>
2015-05-26 22:42:37 +0100

    Simplify Colors

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -1,16 +1,16 @@
 import Color from 'color';
 
+export const trans = function(color: string) {
+    const c = new Color(color);
+    return c.clearer(0.5).rgbString();
+  };
+
 const colors: { [key: string]: string } = {
   background: '#333',
   base: '#ccc',
   theme: '#8800ff',
-  transparent: 'rgba(0,0,0,0)',
-  translucent: 'rgba(0,255,255,0.5)',
-
-  trans: function(color: string) {
-    const c = new Color(color);
-    return c.clearer(0.5).rgbString();
-  }
+  transparent: 'rgba(0,0,0,0)'
 };
+colors.transBase = trans(colors.base);
 
 export default colors;
