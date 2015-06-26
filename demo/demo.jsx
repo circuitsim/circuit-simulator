271bce49ed1f281f689dfdc07ed631912091c39a
ThomWright <github@thomwright.co.uk>
2015-06-26 14:53:37 +0100

    Update dependencies, stop using scss unnecessarily

--- demo/demo.jsx
+++ demo/demo.jsx
@@ -6,8 +6,6 @@ import React from 'react';
 
 import {FullWindow, CircuitDiagram} from '../src/main.js';
 
-import './styles.scss';
-
 const rootInstance = React.render(
   <FullWindow>
     <CircuitDiagram />
