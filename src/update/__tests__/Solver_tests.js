baee180a80dd32b9b6631c031f9974c279c07316
Thom Wright <github@thomwright.co.uk>
2015-07-28 14:35:21 +0100

    Test path problem error condition for solveCircuit

--- src/update/__tests__/Solver_tests.js
+++ src/update/__tests__/Solver_tests.js
@@ -1,5 +1,8 @@
 import {getCircuitInfo, solveCircuit} from '../Solver.js';
-import {BASIC_CIRCUIT} from './CircuitData.js';
+import {BASIC_CIRCUIT, NO_CURRENT_PATH} from './CircuitData.js';
+
+function isTruthy(x) { return x ? true : false; }
+function isFalsy(x) { return !isTruthy(x); }
 
 describe('getCircuitInfo()', () => {
   it('should return the number of nodes for a basic circuit', () => {
@@ -18,13 +21,19 @@ describe('getCircuitInfo()', () => {
 describe('solveCircuit', () => {
   it('should solve a basic circuit', () => {
     const {solution, error} = solveCircuit(BASIC_CIRCUIT, getCircuitInfo(BASIC_CIRCUIT));
-    expect(error).to.equal(undefined);
+    expect(error).to.satisfy(isFalsy);
     expect(solution).to.deep.equal([ 5, 5, 0, 0.5, 0.5 ]); // 5V, 5V, 0V, 0.5A, 0.5A
   });
 
-  it('should return an error if there is a path problem');
+  it('should return an error if there is a path problem', () => {
+    const {error} = solveCircuit(NO_CURRENT_PATH, getCircuitInfo(NO_CURRENT_PATH));
+    expect(error).to.satisfy(isTruthy);
+  });
 
-  it('should return a blank solution if there is a path problem');
+  it('should return a blank solution if there is a path problem', () => {
+    const {solution} = solveCircuit(NO_CURRENT_PATH, getCircuitInfo(NO_CURRENT_PATH));
+    expect(solution).to.deep.equal([ 0 ]);
+  });
 
   it('should return an error for an unsolvable circuit');
 
