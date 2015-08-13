08f39f51c6b2dc3b611d49ff0647e61105eee8bf
Thom Wright <github@thomwright.co.uk>
2015-08-13 13:51:05 +0100

    Add ComponentSelector

--- demo/demo.js
+++ demo/demo.js
@@ -3,17 +3,32 @@
 'use strict';
 
 import React from 'react';
+import { Style } from 'radium';
 // import FullScreen from 'react-fullscreen';
 import Theme from 'circuitsim-theme';
-// import ComponentSelector from 'circuitsim-component-selector';
+import ComponentSelector from 'circuitsim-component-selector';
 import CircuitDiagram from '../src/CircuitDiagram.js';
+
+const COLORS = Theme.COLORS;
+const {lineHeight, fontSize, fontFamily} = Theme.TYPOGRAPHY;
 
 // expose for devTools
 window.React = React;
 
-    // <ComponentSelector theme={ Theme }  />
 React.render(
   <div >
+    <Style
+      rules={{
+        '*': {
+          lineHeight,
+          fontSize,
+          fontFamily,
+          backgroundColor: COLORS.background,
+          color: COLORS.base
+        }
+      }}
+    />
+    <ComponentSelector theme={ Theme }  />
     <CircuitDiagram theme={ Theme }  />
   </div>,
   document.getElementById('circuitsim')
