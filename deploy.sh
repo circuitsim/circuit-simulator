025a8734bb3af7275ddaec840bcb6373e0823457
Thom Wright <github@thomwright.co.uk>
2015-09-03 23:01:54 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+# Check for uncommitted changes or untracked files
+[ -n "$(git status --porcelain)" ] && git status && exit 1
+
+# Switch to gh-pages branch without changing any files
+git symbolic-ref HEAD refs/heads/gh-pages
+git reset
+
+# Add all changes in the build dir
+git --work-tree=build add -A
+git --work-tree=build commit -m "Published changes"
+
+# Switch back to master
+git symbolic-ref HEAD refs/heads/master
+git reset
+
+# Push both branches to GitHub
+git push --all origin
