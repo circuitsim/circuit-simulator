9fefb09ed52dbc4aaa92d5e65ca2fd026c9722fa
Thom Wright <github@thomwright.co.uk>
2015-10-18 12:28:07 +0100

    Add redux-logger middleware. Fixes #23

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,33 @@
+import R from 'ramda';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers';
 
+import {
+  LOOP_BEGIN,
+  LOOP_UPDATE,
+  ADDING_MOVE,
+  MOVING_MOVE,
+  MOUSE_MOVED
+} from './actions.js';
+import createLogger from 'redux-logger';
+
+const logger = createLogger({
+  predicate: (getState, action) => {
+    return R.all(t => t !== action.type)([
+      LOOP_BEGIN,
+      LOOP_UPDATE,
+      ADDING_MOVE,
+      MOVING_MOVE,
+      MOUSE_MOVED
+    ]);
+  },
+  collapsed: true
+});
+
 const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware
+  thunkMiddleware,
+  logger
 )(createStore);
 
 export default function configureStore() {
