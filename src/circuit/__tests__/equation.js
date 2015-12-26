a3bb55d0556f4e8d3e1aa477559ec8df2334c9db
Thom Wright <github@thomwright.co.uk>
2015-12-26 17:02:44 +0000

    Add new stamping functions

---
+++ src/circuit/__tests__/equation.js
@@ -0,0 +1,161 @@
+import {Arrays as Matrixy} from 'matrixy';
+import {
+  createBlankEquation,
+  stampResistor,
+  stampVoltageSource,
+  stampCurrentSource,
+  // clone,
+  solve
+} from '../equation';
+
+const {createBlank} = Matrixy;
+
+const blankThreeNodeEquation = {
+  nodalAdmittances: createBlank(2, 2),
+  inputs: createBlank(2, 1),
+  numOfVSourcesStamped: 0,
+  numOfVSources: 1,
+  numOfNodes: 2
+};
+
+describe('Equation Builder initialisation', function() {
+  it('should initialise a blank equation', () => {
+    const equation = createBlankEquation({
+      numOfNodes: 2,
+      numOfVSources: 1
+    });
+    expect(equation).to.eql(blankThreeNodeEquation);
+  });
+
+  it('should throw an exception if given a number of nodes < 2', function() {
+    expect(() => {
+      createBlankEquation({
+        numOfNodes: 1,
+        numOfVSources: 0
+      });
+    }).to.throw(/.*Number of nodes.*/);
+  });
+});
+
+describe('Stamping:', function() {
+  describe('stamping a resistance', function() {
+    it('should stamp a resistance into the nodal admittance matrix', function() {
+      const equation = createBlankEquation({ numOfNodes: 3, numOfVSources: 0 });
+      stampResistor(equation)(5, 1, 2);
+      const {nodalAdmittances, inputs} = equation;
+      expect(nodalAdmittances).to.eql([[1 / 5, -1 / 5],
+                                       [-1 / 5, 1 / 5]]);
+      expect(inputs).to.eql(blankThreeNodeEquation.inputs);
+    });
+
+    it('should be additive', function() {
+      const equation = createBlankEquation({ numOfNodes: 3, numOfVSources: 0 });
+      stampResistor(equation)(5, 0, 2);
+      stampResistor(equation)(5, 1, 2);
+      expect(equation.nodalAdmittances).to.eql([[1 / 5, -1 / 5],
+                                                [-1 / 5, 2 / 5]]);
+    });
+
+    it('should throw an exception if resistance is zero', function() {
+      const equation = createBlankEquation({ numOfNodes: 3, numOfVSources: 0 });
+      expect(() => {
+        stampResistor(equation)(0, 1, 2);
+      }).to.throw(/.*Resistance.*/);
+    });
+
+    it('should not stamp a negative resistance', function() {
+      const equation = createBlankEquation({ numOfNodes: 3, numOfVSources: 0 });
+      expect(() => {
+        stampResistor(equation)(-1, 1, 2);
+      }).to.throw(/.*Resistance.*/);
+    });
+  });
+
+  describe('stamping a voltage source', function() {
+    it('should stamp a voltage into the input vector', function() {
+      const equation = createBlankEquation({
+        numOfNodes: 3,
+        numOfVSources: 1
+      });
+      stampVoltageSource(equation)(5, 1, 2);
+      expect(equation.inputs).to.eql([[0],
+                                      [0],
+                                      [5]]);
+    });
+
+    it('should stamp into the augmented part of the nodal admittance matrix', function() {
+      const equation = createBlankEquation({
+        numOfNodes: 3,
+        numOfVSources: 1
+      });
+      stampVoltageSource(equation)(5, 1, 2);
+      expect(equation.nodalAdmittances).to.eql([[0, 0, 1],
+                                                [0, 0, -1],
+                                                [-1, 1, 0]]);
+    });
+
+    it('should not stamp more than the specified number of voltage sources', function() {
+      const equation = createBlankEquation({
+        numOfNodes: 3,
+        numOfVSources: 1
+      });
+      stampVoltageSource(equation)(5, 0, 1);
+      expect(() => {
+        stampVoltageSource(equation)(5, 0, 1);
+      }).to.throw(/.*number of voltage sources.*/);
+    });
+  });
+
+  describe('stamping a current source', function() {
+    it('should stamp a current source', function() {
+      const equation = createBlankEquation({
+        numOfNodes: 3,
+        numOfVSources: 0
+      });
+      stampCurrentSource(equation)(5, 1, 2);
+      const {nodalAdmittances, inputs} = equation;
+      expect(inputs).to.eql([[-5],
+                             [5]]);
+      expect(nodalAdmittances).to.eql(blankThreeNodeEquation.nodalAdmittances);
+    });
+  });
+});
+
+describe('solve', function() {
+  it('should solve a simple circuit with no voltage sources', function() {
+    const equation = createBlankEquation({
+      numOfNodes: 2,
+      numOfVSources: 0
+    });
+    stampCurrentSource(equation)(1, 0, 1);
+    stampResistor(equation)(100, 1, 0);
+    const solution = solve(equation);
+    expect(solution).to.eql([[100]]);
+  });
+
+  it('should solve a simple circuit with a volage source', function() {
+    const equation = createBlankEquation({
+      numOfNodes: 2,
+      numOfVSources: 1
+    });
+    stampVoltageSource(equation)(10, 0, 1); // wire
+    stampResistor(equation)(10, 1, 0);
+    const solution = solve(equation);
+    expect(solution).to.eql([[10],
+                             [1]]);
+  });
+
+  it('should solve a simple circuit with a wire', function() {
+    const equation = createBlankEquation({
+      numOfNodes: 3,
+      numOfVSources: 1
+    });
+    stampCurrentSource(equation)(1, 0, 1);
+    stampVoltageSource(equation)(0, 1, 2); // wire
+    stampResistor(equation)(100, 2, 0);
+    const solution = solve(equation);
+    expect(solution).to.eql([[100],
+                             [100],
+                             [1]]);
+  });
+});
---
+++ src/circuit/equation.js
@@ -0,0 +1,77 @@
+import {Arrays as Matrixy} from 'matrixy';
+import assert from 'assert';
+
+const MIN_NUM_OF_NODES = 2;
+
+export const createBlankEquation = ({numOfNodes, numOfVSources}) => {
+  assert(numOfNodes >= MIN_NUM_OF_NODES, `Number of nodes must be >= ${MIN_NUM_OF_NODES}, was: ${numOfNodes}`);
+
+  const size = numOfNodes + numOfVSources - 1;
+  return {
+    nodalAdmittances: Matrixy.createBlank(size),
+    inputs: Matrixy.createBlank(size, 1),
+    numOfVSourcesStamped: 0,
+    numOfVSources,
+    numOfNodes
+  };
+};
+
+export const clone = equation => {
+  return {
+    ... equation,
+    nodalAdmittances: Matrixy.copy(equation.nodalAdmittances),
+    inputs: Matrixy.copy(equation.inputs)
+  };
+};
+
+export const solve = equation => Matrixy.solve(equation.nodalAdmittances, equation.inputs);
+
+// NOTE: All functions below mutate the equation.
+
+const stampNodalAdmittanceMatrix = equation => (row, col, x) => {
+  if (row !== 0 && col !== 0) { // ignore ground node
+    row--;
+    col--;
+    equation.nodalAdmittances[row][col] += x;
+  }
+};
+
+const stampInputVector = equation => (row, x) => {
+  if (row !== 0) {
+    row--;
+    equation.inputs[row][0] += x;
+  }
+};
+
+const stampConductance = equation => (conductance, node1, node2) => {
+  stampNodalAdmittanceMatrix(equation)(node1, node1, conductance);
+  stampNodalAdmittanceMatrix(equation)(node2, node2, conductance);
+  stampNodalAdmittanceMatrix(equation)(node1, node2, -conductance);
+  stampNodalAdmittanceMatrix(equation)(node2, node1, -conductance);
+};
+
+export const stampResistor = equation => (resistance, node1, node2) => {
+  assert(resistance > 0, 'Resistance must be > 0, was: ' + resistance);
+
+  const conductance = 1 / resistance;
+  stampConductance(equation)(conductance, node1, node2);
+};
+
+export const stampVoltageSource = equation => (voltage, fromNode, toNode) => {
+  const {numOfVSources, numOfVSourcesStamped} = equation;
+  assert(numOfVSourcesStamped < numOfVSources, `Already stamped declared number of voltage sources (${numOfVSources})`);
+
+  const vIndex = equation.numOfNodes + equation.numOfVSourcesStamped;
+  equation.numOfVSourcesStamped++;
+  stampNodalAdmittanceMatrix(equation)(vIndex, fromNode, -1);
+  stampNodalAdmittanceMatrix(equation)(vIndex, toNode, 1);
+  stampNodalAdmittanceMatrix(equation)(fromNode, vIndex, 1);
+  stampNodalAdmittanceMatrix(equation)(toNode, vIndex, -1);
+  stampInputVector(equation)(vIndex, voltage);
+  return vIndex;
+};
+
+export const stampCurrentSource = equation => (current, fromNode, toNode) => {
+  stampInputVector(equation)(fromNode, -current);
+  stampInputVector(equation)(toNode, current);
+};
