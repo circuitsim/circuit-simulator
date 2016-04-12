39c3f3ad372899710e70e55736b95e68149890d6
Thom Wright <github@thomwright.co.uk>
2016-04-12 20:09:07 +0100

    Remove leftover react-art stuff

--- src/ui/utils/DrawingUtils.js
+++ src/ui/utils/DrawingUtils.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import {Path} from 'react-art';
 import Vector from 'immutable-vector2d';
 import evPos from 'ev-pos';
 
@@ -25,12 +23,6 @@ const utils = {
     return evPos(event.nativeEvent || event, node);
   },
 
-  makeArtListener: function(f) {
-    return {
-      handleEvent: f
-    };
-  },
-
   getRectPointsBetween(p1: Vector, p2: Vector, width: number) {
     const v = utils.diff(p1, p2);
     const p = { // perpendicular
@@ -52,57 +44,6 @@ const utils = {
     ];
 
     return cs;
-  },
-
-  getRectPathBetween(p1: Vector, p2: Vector, width: number) {
-    const cs = utils.getRectPointsBetween(p1, p2, width);
-    return new Path()
-        .moveTo(cs[0].x, cs[0].y)
-        .lineTo(cs[1].x, cs[1].y)
-        .lineTo(cs[2].x, cs[2].y)
-        .lineTo(cs[3].x, cs[3].y)
-        .close();
-  },
-
-  drawLine(p1: Vector, p2: Vector) {
-    return new Path()
-        .moveTo(p1.x, p1.y)
-        .lineTo(p2.x, p2.y)
-        .close();
-  },
-
-  drawCenteredCircle(p: Vector, radius: number) {
-    const offset = {
-      x: radius,
-      y: 0
-    };
-    const [p1, p2] = [p.subtract(offset), p.add(offset)];
-    return utils.drawCircle(p1, p2);
-  },
-
-  drawCircle(p1: Vector, p2: Vector) {
-    const v = utils.diff(p1, p2);
-    const radius = v.length() / 2;
-
-    return new Path()
-        .moveTo(p1.x, p1.y)
-        .arc(-v.x, -v.y, radius, radius)
-        .arc(v.x, v.y, radius, radius)
-        .close();
-  },
-
-  PropTypes: {
-    ArtListener: React.PropTypes.shape({
-      handleEvent: React.PropTypes.func.isRequired
-    }),
-    Vector: React.PropTypes.shape({
-      x: React.PropTypes.number.isRequired,
-      y: React.PropTypes.number.isRequired
-    })
-  },
-
-  Colors: {
-    transparent: 'rgba(0,0,0,0)'
   }
 };
 
